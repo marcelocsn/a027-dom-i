11b9f1e25db299dbb2d21d83496e15f86cebeb50
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-10-03 15:53:53 -0300

    Aula 27 realizada dia 03-10-22

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let fruta1 = document.getElementById("fruta-1");
+fruta1.innerHTML= frutas[0]
+
+let fruta2 = document.getElementById("fruta-2");
+fruta2.innerHTML= frutas[1]
+
+let fruta3 = document.getElementById("fruta-3");
+fruta3.innerHTML= frutas[2]
+
+let printaFrutaNoConsole = document.getElementById("fruta")
+
+function imprimeFruta() {
+    console.log(printaFrutaNoConsole.value)
+}
+function aparecerNaTela() {
+    let fruta4 = document.getElementById("fruta-4")
+    fruta4.innerHTML = printaFrutaNoConsole.value
+}
